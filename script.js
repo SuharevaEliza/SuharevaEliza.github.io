17e613c83c7616212a1c2720735223505592d4c7
SuharevaEliza <eliza@dynamicyield.com>
2019-08-23 17:16:07 +0300

    Add SW Registration

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('hello from script');
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('sw.js').then(function(registration) {
