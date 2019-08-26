4cfe08b12d2c68d14b464da8ef693db9ff893e46
SuharevaEliza <eliza@dynamicyield.com>
2019-08-26 10:36:06 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log('script enabled');
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('sw.js').then(function(registration) {
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var  CACHE_NAME = 'myTestCache';
+var CACHE_NAME = 'myTestCache';
 var urlsToCache = [
     '/index.html',
     '/pages/recommendations.html',
@@ -6,6 +6,7 @@ var urlsToCache = [
     '/script.js'
 ];
 
+// INSTALLATION
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
@@ -15,3 +16,42 @@ self.addEventListener('install', function(event) {
             })
     );
 });
+
+
+// RETURNING REQUESTS
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+
+                // if any match in cache found
+                if(response) {
+                    return response;
+                }
+
+                //otherwise return result of fetch (makes a network request, returns anything that can be received)
+                return fetch(event.request)
+                    .then(function (response) {
+
+                        // ensure the response is valid
+                        // ('basic' indicates a request from our origin - requests to 3rd party are not cached)
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response
+                        }
+
+                        // clone the response (to capture it from the stream) -
+                        // we want to pass it to browser and cache
+                        var responseToCache = response.clone();
+
+
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache)
+                            });
+
+                        return response;
+                    })
+            })
+    )
+});
+
