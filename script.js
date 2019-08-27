a555c304901cf93ee39a41ad9f62ab360b8a4ea1
SuharevaEliza <eliza@dynamicyield.com>
2019-08-27 11:30:39 +0300

    fix

--- script.js
+++ script.js
@@ -12,12 +12,30 @@ if ('serviceWorker' in navigator) {
 }
 
 var button = document.querySelector('#button');
-button.addEventListener('click', sendEvent);
+button.addEventListener('click', sender);
 
-function sendEvent() {
-    localforage.setItem('somekey', 'my xml request').then(function (value) {
-        console.log('set value: '+ value);
-    }).catch(function(err) {
-        console.log(err);
+function sender(){
+    var oReq = new XMLHttpRequest();
+    var url = 'https:​ //dy-api.com/v2​ /collect/user/event';
+    var body = {
+        'user' : {
+            'id' : '5273715487327152493'
+        },
+        'sessionId' : 'e89f7ab43ce281248b090bd460d7a1ac',
+        'events' : [
+            {
+                'name' : 'Clicks fired',
+                'properties' : {}
+            }
+        ]
+    };
+
+    oReq.addEventListener('loadend', function() {
+        console.log(oReq.responseText);
     });
+
+    oReq.open('POST', url);
+    oReq.setRequestHeader('DY-API-Key​', 'c1a70218059b474d483a6a2d41b961ee455f7734f7575f34aa85a8f3e3f8a60e');
+    oReq.setRequestHeader('Content-Type', 'application/json');
+    oReq.send(body);
 }
