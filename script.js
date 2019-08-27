8e8a95df44235cf3d69ef1756678dd75d98a3c15
SuharevaEliza <eliza@dynamicyield.com>
2019-08-27 16:42:58 +0300

    fix

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('oy');
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker.register('sw.js').then(function(registration) {
@@ -9,4 +7,30 @@ if ('serviceWorker' in navigator) {
             console.log('ServiceWorker registration failed: ', err);
         });
     });
+}
+
+var buttons = [].slice.call(document.querySelectorAll('#clicks button'));
+buttons.forEach(function(button){
+    button.addEventListener('click', sendRequest);
+});
+
+
+function sendRequest(){
+    var url = 'https://enh6y4mdg615v.x.pipedream.net/';
+    var method = 'GET';
+    var oReq = new XMLHttpRequest();
+
+    oReq.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var message = document.querySelector("#message");
+            message.style.setProperty('display', 'block');
+            message.innerHTML = oReq.responseText;
+            setTimeout(function () {
+                message.style.setProperty('display', 'none');
+            }, 2500)
+        }
+    };
+
+    oReq.open(method, url);
+    oReq.send('');
 }
