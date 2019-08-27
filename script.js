aad63ae38f5af033b73c0fe89efe3e9f91c9ba71
SuharevaEliza <eliza@dynamicyield.com>
2019-08-27 14:31:06 +0300

    fix

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-
-var map = document.querySelector('#map');
-
 function initMap() {
     var location = {lat: -25.344, lng: 131.036};
     var map = new google.maps.Map(
