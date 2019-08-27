ffa33ce87d736361e70066d92218d2481c92d68b
SuharevaEliza <eliza@dynamicyield.com>
2019-08-27 11:50:08 +0300

    fix

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ var button = document.querySelector('#button');
 button.addEventListener('click', sender);
 
 function sender(){
-    var oReq = new XMLHttpRequest();
-    var url = 'https:​ //dy-api.com/v2​ /collect/user/event';
+    var url = 'https:​//dy-api.com/v2​/collect/user/event';
     var body = {
         'user' : {
             'id' : '5273715487327152493'
@@ -35,6 +34,7 @@ function sender(){
     });
 
     oReq.open('POST', url);
+    oReq.withCredentials = true;
     oReq.setRequestHeader('DY-API-Key', 'c1a70218059b474d483a6a2d41b961ee455f7734f7575f34aa85a8f3e3f8a60e');
     oReq.setRequestHeader('Content-Type', 'application/json');
     oReq.send(body);
