861f189daf418d88d67a1edafe81d34aafe7686c
SuharevaEliza <eliza@dynamicyield.com>
2019-09-02 09:14:01 +0300

    getting recs

--- pages/ssapi.js
+++ pages/ssapi.js
@@ -1,40 +1,40 @@
-var userId = document.querySelector('meta[property="dyapi:userid"]').content;
-var sessionID = document.querySelector('meta[property="dyapi:sessionid"]').content;
+// var userId = document.querySelector('meta[property="dyapi:userid"]').content;
+// var sessionID = document.querySelector('meta[property="dyapi:sessionid"]').content;
 
 callRecommendations();
 
 function callRecommendations(){
-    var body = {
-        "user": {
-            "id": userId
+    var userID = 'u62d986ab7e';
+    var sessionID = 'sf65a42c18de';
+
+    var data = {
+      user: {
+          id : userID
+      },
+      sessionId : sessionID,
+        selector : {
+          names: ['API recs']
         },
-        "sessionId": sessionID,
-        "selector": {
-            "names": ["API recs"]
-        },
-        "context": {
-            "page": {
-                "type": "HOMEPAGE",
-                "data": [],
-                "location": "https://suharevaeliza.github.io/pages/contacts.html"
-            },
-            "device": {
-                "ip": "172.17.0.1",
-                "userAgent": "Mozilla/5.0 (X11; ; U; Linux armv7l; en-us)"
-            }
+        context : {
+          page : {
+              type : 'HOMEPAGE',
+              data : [],
+              location : 'https://suharevaeliza.github.io/pages/contacts.html'
+          }
         }
-    }
+    };
 
-    return fetch(`https://direct.dy-api.com/v2/serve/user/choose`, {
-        method: 'POST',
-        headers: {
-            'DY-API-Key': `f205138651b370352c58fa1e88e0da801fc2b1f4a1050c60f4aa346d1e36166a`,
-            'Content-Type': 'application/json',
-        },
-        cache: 'no-cache',
-        body: JSON.stringify(body)
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
     });
+
+    xhr.open("POST", "https://cors-anywhere.herokuapp.com/https://dy-api.com/v2/serve/user/choose");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("dy-api-key", "f205138651b370352c58fa1e88e0da801fc2b1f4a1050c60f4aa346d1e36166a");
+
+    xhr.send(JSON.stringify(data));
 }
-
-
-
