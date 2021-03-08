b43534663838cd185608f013a717a5386141bfb8
SuharevaEliza <eliza@dynamicyield.com>
2021-03-08 15:15:40 +0100

    script

--- crm/script.js
+++ crm/script.js
@@ -1,6 +1,6 @@
 var DROPDOWN_VALUES = {
-    nullValues : ['string', 'int', 'long', 'double', 'date'],
-    textValues: ['bool', 'bucket', 'enum', 'array']
+    nullValues: ['string', 'int', 'long', 'double', 'date'],
+    textValues: ['boolean', 'bucket', 'enum', 'array']
 };
 var TABLE = document.getElementById('table-body');
 var ROW_TEMPLATE = document.getElementById('row-template');
@@ -27,7 +27,7 @@ function populateOptions() {
     });
 }
 
-function addNewRow(){
+function addNewRow() {
     var newRow = ROW_TEMPLATE.cloneNode(true);
     newRow.classList.remove('row-template');
     newRow.classList.add('crm-row');
@@ -40,10 +40,10 @@ function addNewRow(){
     createDataObject(ROW_COUNT);
 }
 
-function createDataObject(row){
+function createDataObject(row) {
     var rowElements = 5;
     DATA_COLLECTION[row] = new Array();
-    for(i=0; i<rowElements; i++){
+    for (i = 0; i < rowElements; i++) {
         DATA_COLLECTION[row].push('NULL');
     }
 }
@@ -55,7 +55,7 @@ function configureNewRow(row) {
     addInputListener(row);
 }
 
-function addRemoveRowHandler(row){
+function addRemoveRowHandler(row) {
     var removeButton = row.querySelector('.remove');
 
     displayRemoveRowButtons();
@@ -65,9 +65,9 @@ function addRemoveRowHandler(row){
     });
 }
 
-function displayRemoveRowButtons(){
+function displayRemoveRowButtons() {
     var buttons = [].slice.call(TABLE.querySelectorAll('.crm-row .remove'));
-    if(buttons.length > 1){
+    if (buttons.length > 1) {
         buttons.forEach(button => {
             button.classList.remove('hidden');
         })
@@ -75,15 +75,15 @@ function displayRemoveRowButtons(){
         buttons[0].classList.add('hidden');
     }
 }
- function addRemoveFunctionality(row) {
+function addRemoveFunctionality(row) {
     var buttons = [].slice.call(TABLE.querySelectorAll('.crm-row .remove'))
-    if(buttons.length >1 ){
+    if (buttons.length > 1) {
         row.parentElement.removeChild(row);
-        Object.keys(DATA_COLLECTION).forEach((ind)=>{
-            if(ind.indexOf(row.dataset.index)>-1) delete DATA_COLLECTION[ind];
+        Object.keys(DATA_COLLECTION).forEach((ind) => {
+            if (ind.indexOf(row.dataset.index) > -1) delete DATA_COLLECTION[ind];
         });
     }
- }
+}
 
 function addSelectionListener(row) {
     var select = row.querySelector('select');
@@ -92,12 +92,12 @@ function addSelectionListener(row) {
 
     select.addEventListener('change', () => {
         var selectedValue = select.selectedOptions[0].value;
-        if(selectedValue === 'bool'){
+        if (selectedValue === 'boolean') {
             removeCustomOptions(row);
             setBoolOption(sourceValue, displayValue);
             disableAddingOption(row);
         }
-        else if(DROPDOWN_VALUES.textValues.indexOf(selectedValue) > -1){
+        else if (DROPDOWN_VALUES.textValues.indexOf(selectedValue) > -1) {
             toggleInputValueFields(sourceValue, displayValue, false, '');
             removeBoolOptions(row);
             enableAddingOption(row);
@@ -110,30 +110,28 @@ function addSelectionListener(row) {
     })
 }
 
-function addInputListener(row, source, display){
+function addInputListener(row, source, display) {
     var actions = {
-        'INPUT':'keyup',
-        'SELECT':'change'
+        'INPUT': 'keyup',
+        'SELECT': 'change'
     }
     var objectID = row;
-    if(row.id) objectID = row.dataset.index;
+    if (row.id) objectID = row.dataset.index;
     var inputs = [];
-    
-    if(!source && !display && row.id) {
+
+    if (!source && !display && row.id) {
         inputs = row.querySelectorAll('input, select');
-    } else if (display) {
-        inputs = [,,,source, display];
     } else {
-        inputs = [,,,source,];
+        inputs = [, , , source, display];
     }
 
     inputs.forEach((el, index) => {
         el.addEventListener(actions[el.tagName], () => {
             el.classList.remove('invalid');
-            Object.keys(DATA_COLLECTION).forEach((ind)=>{
-                if(objectID.indexOf('/') < 0){
+            Object.keys(DATA_COLLECTION).forEach((ind) => {
+                if (objectID.indexOf('/') < 0) {
                     DATA_COLLECTION[objectID][index] = el.value;
-                    if(ind.split('/')[0] === objectID && index < 3) {
+                    if (ind.split('/')[0] === objectID && index < 3) {
                         DATA_COLLECTION[ind][index] = el.value;
                     }
                 } else {
@@ -144,9 +142,9 @@ function addInputListener(row, source, display){
     });
 }
 
-function toggleInputValueFields(sourceValue, displayValue, disabled, placeholder){
+function toggleInputValueFields(sourceValue, displayValue, disabled, placeholder) {
     var rowID = parseInt(getParent(sourceValue, '.crm-row').id.replace('row-', ''));
-    DATA_COLLECTION[rowID][3] =  DATA_COLLECTION[rowID][4] = placeholder;
+    DATA_COLLECTION[rowID][3] = DATA_COLLECTION[rowID][4] = placeholder;
 
     sourceValue.disabled = disabled;
     sourceValue.placeholder = sourceValue.value = placeholder;
@@ -155,7 +153,7 @@ function toggleInputValueFields(sourceValue, displayValue, disabled, placeholder
     displayValue.placeholder = displayValue.value = placeholder;
 }
 
-function setBoolOption(sourceValue, displayValue){
+function setBoolOption(sourceValue, displayValue) {
     var row = getParent(sourceValue, '.crm-row')
     var sourceContainer = sourceValue.parentElement;
     var displayContainer = displayValue.parentElement;
@@ -163,28 +161,36 @@ function setBoolOption(sourceValue, displayValue){
     var newDisplayContainer = displayContainer.cloneNode(true);
     var newSourceValue = newSourceContainer.querySelector('input');
     var newDisplayValue = newDisplayContainer.querySelector('input');
+
+    sourceContainer.classList.add('bool');
     newSourceContainer.classList.add('bool');
     newSourceContainer.dataset.index = 1;
+
+    displayContainer.classList.add('bool');
     newDisplayContainer.classList.add('bool');
     newDisplayContainer.dataset.index = 1;
 
     sourceValue.value = 'True';
     newSourceValue.value = 'False';
-    sourceContainer.parentElement.appendChild(newSourceContainer);
+
     displayValue.disabled = newDisplayValue.disabled = false;
     sourceValue.disabled = newSourceValue.disabled = true;
+
     displayValue.value = newDisplayValue.value = displayValue.placeholder = newDisplayValue.placeholder = '';
+
+    sourceContainer.parentElement.appendChild(newSourceContainer);
     displayContainer.parentElement.appendChild(newDisplayContainer);
 
     var dataObjectId = row.dataset.index + '/1';
     createDataObject(dataObjectId);
     DATA_COLLECTION[dataObjectId] = createOptionDataSet(row);
-    addInputListener(row, sourceValue);
-    addInputListener(row, newSourceValue);
+    DATA_COLLECTION[row.dataset.index][3] = "TRUE";
+    DATA_COLLECTION[dataObjectId][3] = "FALSE";
+    addInputListener(dataObjectId, newSourceValue, newDisplayValue);
 }
 
-function enableAddingOption(row){
-    if(!row.querySelector('.add-option-container')){
+function enableAddingOption(row) {
+    if (!row.querySelector('.add-option-container')) {
         var newOptionButton = ADD_OPTION_TEMPLATE.cloneNode(true);
         newOptionButton.classList.remove('template');
         newOptionButton.classList.remove('removed');
@@ -193,24 +199,24 @@ function enableAddingOption(row){
     }
 }
 
-function addNewOption(){
+function addNewOption() {
     var row = getParent(this, '.crm-row');
     var sourceValues = [].slice.call(row.querySelectorAll('.source-value-container'));
     var displayValues = [].slice.call(row.querySelectorAll('.display-value-container'));
-    
+
     var newSourceValue = sourceValues[0].cloneNode(true);
     var newSourceInput = newSourceValue.querySelector('input');
     newSourceValue.classList.add('option');
     newSourceValue.dataset.index = displayValues.length;
     newSourceInput.value = '';
-    
+
     var newDisplayValue = displayValues[0].cloneNode(true);
     var newDisplayInput = newDisplayValue.querySelector('input')
     newDisplayValue.classList.add('option');
     newDisplayValue.dataset.index = displayValues.length;
     newDisplayValue.querySelector('button').classList.remove('hidden');
     newDisplayInput.value = '';
-    
+
     sourceValues[0].parentElement.appendChild(newSourceValue);
     displayValues[0].parentElement.appendChild(newDisplayValue);
 
@@ -221,14 +227,14 @@ function addNewOption(){
     setRemoveOptionListener(row);
 }
 
-function setRemoveOptionListener(row){
+function setRemoveOptionListener(row) {
     var removeOptionButtons = row.querySelectorAll('.display-value-container--remove');
     [].slice.call(removeOptionButtons).forEach(button => {
         button.addEventListener('click', removeOption);
     })
 }
 
-function removeOption(){
+function removeOption() {
     var opntionId = this.parentElement.dataset.index;
     var row = getParent(this, '.crm-row');
     var dataObjectId = [row.dataset.index, opntionId].join('/');
@@ -238,38 +244,38 @@ function removeOption(){
     })
 }
 
-function createOptionDataSet(row){
+function createOptionDataSet(row) {
     var displayName = row.querySelector('[name="display-name"]').value;
     var columnName = row.querySelector('[name="column-name"]').value;
     var type = row.querySelector('[name="data-type"]').value;
     var sourceValues = row.querySelectorAll('.source-value');
     var displayValues = row.querySelectorAll('.display-value');
-    var sourceValue = sourceValues[sourceValues.length-1].value;
-    var displayValue = displayValues[sourceValues.length-1].value;
+    var sourceValue = sourceValues[sourceValues.length - 1].value;
+    var displayValue = displayValues[sourceValues.length - 1].value;
 
     return [displayName, columnName, type, sourceValue, displayValue];
 }
 
-function disableAddingOption(row){
+function disableAddingOption(row) {
     var addButton = row.querySelector('.add-option-container')
-    if(addButton) addButton.parentElement.removeChild(addButton);
+    if (addButton) addButton.parentElement.removeChild(addButton);
 }
 
-function removeBoolOptions(row){
+function removeBoolOptions(row) {
     [].slice.call(row.querySelectorAll('.bool')).forEach((el) => {
-        if(el) el.parentElement.removeChild(el);
+        if (el) el.parentElement.removeChild(el);
     })
 }
 
-function removeCustomOptions(row){
+function removeCustomOptions(row) {
     [].slice.call(row.querySelectorAll('.option')).forEach((el) => {
-        if(el) el.parentElement.removeChild(el);
+        if (el) el.parentElement.removeChild(el);
     })
 }
 
-function setButtonListeners(){
+function setButtonListeners() {
     document.querySelectorAll('#add-row, #export-file').forEach((button) => {
-        if(button.id === 'add-row'){
+        if (button.id === 'add-row') {
             button.addEventListener('click', addNewRow);
         } else {
             button.addEventListener('click', generateFile);
@@ -277,14 +283,14 @@ function setButtonListeners(){
     })
 }
 
-function generateFile(){
-    if(validateInputs() === true){
+function generateFile() {
+    if (validateInputs() === true) {
         var fileHeader = 'Field_Name,Display_Field_Name,Type,Dropdown_Source_Value,Dropdown_Display_Value\n';
         var sortedData = sortObject(DATA_COLLECTION);
-        
+
         var text = fileHeader + sortedData.map((array) => {
             array[1].map(value => {
-                if(value === 'undefined' || value === "") return 'NULL';
+                if (value === 'undefined' || value === "") return 'NULL';
             });
             return array[1].join(',');
         }).join('\n');
@@ -300,8 +306,8 @@ function generateFile(){
     }
 }
 
-function sortObject(object){
-    return Object.entries(object).sort((a,b) => {
+function sortObject(object) {
+    return Object.entries(object).sort((a, b) => {
         return a[0].split('/')[0] - b[0].split('/')[0]
     })
 }
@@ -313,7 +319,7 @@ function validateInputs() {
             field.classList.add('invalid');
             result = false;
         }
-        if (field.tagName === "INPUT" && field.disabled === false && field.value === ""){
+        if (field.tagName === "INPUT" && field.disabled === false && field.value === "") {
             field.classList.add('invalid');
             result = false;
         }
@@ -334,13 +340,13 @@ function downloadFile(filename, text) {
 
 function getParent(element, selector) {
     var currentElement = element;
-  
+
     do {
-      currentElement = currentElement.parentNode;
-      if (currentElement.matches(selector)) {
-        return currentElement;
-      }
+        currentElement = currentElement.parentNode;
+        if (currentElement.matches(selector)) {
+            return currentElement;
+        }
     } while (currentElement.nodeName.toLowerCase() !== 'body');
-  
+
     return null;
-  }
+}
